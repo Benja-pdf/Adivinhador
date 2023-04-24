5b25a6c8e59826c42671929d86f8783752550e78
Benja-pdf <kauabenjamint@gmail.com>
2023-04-24 11:56:57 -0300

    Aula-1-Impressao_e_leitura

---
+++ jogoAdivinhador.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    int numeroSecreto = 22;
+    int chute;
+
+    printf ("***************************\n");
+    printf ("*Bem vindo ao adivinhador!*\n");
+    printf ("***************************\n");
+
+    printf ("Eai, chuta um numero ");
+    scanf ("%d", &chute);
+    printf ("Voce chutou %d", chute);
+
+}
